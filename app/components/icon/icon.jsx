d0994b9869e9cc04fe956706cc42784457fadab6
Raghav <raghav.duggal.s69@kalvium.community>
2026-08-30 19:01:09 +0530

    Align Remix versions to fix hydration mismatch and fix SVG fragment issue

--- app/components/icon/icon.jsx
+++ app/components/icon/icon.jsx
@@ -1,7 +1,7 @@
 import { classes } from '~/utils/style';
 import styles from './icon.module.css';
 import { forwardRef } from 'react';
-import sprites from './icons.svg';
+import sprites from './icons.svg?url';
 
 export const Icon = forwardRef(({ icon, className, size, ...rest }, ref) => {
   return (
